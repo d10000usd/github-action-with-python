3484647b78dfb61ff6e024f4e50973fb5d67a32b
d10000usd <d10000usd@gmail.com>
2022-12-01 00:34:55 +0900

    first commit

--- github_utils.py
+++ github_utils.py
@@ -11,7 +11,6 @@ def get_github_repo(access_token, repository_name):
     g = Github(access_token)
     return g.get_user().get_repo(repository_name)
 
-
 def upload_github_issue(repo, title, body):
     """
     해당 repo에 title 이름으로 issue를 생성하고, 내용을 body로 채우는 함수
@@ -20,7 +19,4 @@ def upload_github_issue(repo, title, body):
     :param body: issue body
     :return: None
     """
-
     repo.create_issue(title=title, body=body)
-
-
--- test.py
+++ test.py
@@ -1,13 +1,63 @@
-from github import Github
+# -*- coding: utf-8 -*-
+#
+#  test.py
+#  project
+#
+#  Created by hg on 2022-11-30.
+#  Copyright 2022 hg. All rights reserved.
+#
+#  d10000usd@gmail.com
 
-# First create a Github instance:
-
-# using an access token
-g = Github("access_token")
-
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+import json
+import os
+import pandas as pd
+import numpy as np
+import sys
+import pickle
+# import tqdm.tqdm_notebook as tqdm
+# import warnigs
+# warnings.filterwarnings(action='ignore') # Warning Message 안보이게
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+# pd.set_option('display.width', 1000)
+# pd.set_option('display.max_colwidth', -1
+class nameproject:
+    """ docstring
+    """
+    def __init__(self, var,var1):
+        self.var = var
+        self.var1 = var1
+    def __str__(self):
+        return 'name@{:#x}: {}'.format(id(self), self.var)
+    def name_1(self,project, docstring):
+        """
+        docstring
+        :param1 : par - 
+        :param2 : par - 
+        :param3 : par - 
+        :Returns ret -  
+        """
+        self.val = 1 
+        return self.val
+    def name_2(self,project, docstring):
+        """
+        docstring
+        :param1 : par - 
+        :param2 : par - 
+        :param3 : par - 
+        :Returns ret -  
+        """
+        self.val = 1 
+        return self.val
+    @property
+    def name_3(self):
+        return self.name
+    @name.setter
+    def name_4(self, value):
+        self.name = value
+    @name.deleter
+    def name_4(self, value):
+        del self.name
+if __name__ == "__main__":
+    classmethod = nameproject('classmethod')
+    classmethod.name_1()
